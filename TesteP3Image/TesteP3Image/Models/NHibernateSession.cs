edcba35a566542c1f8e149bfcb1de3a6e163dca7
Marco Aurélio Romeu Soares Jr. <marco@nossaweb.com.br>
2014-01-13 07:15:44 -0200

    ajuste

--- TesteP3Image/TesteP3Image/Models/NHibernateSession.cs
+++ TesteP3Image/TesteP3Image/Models/NHibernateSession.cs
@@ -11,19 +11,17 @@ namespace TesteP3Image.Models
         public static ISession OpenSession()
         {
             var configuration = new Configuration();
-            //var configurationPath = HttpContext.Current.Server.MapPath(@"~\Models\NHibernate\Configuration\hibernate.cfg.xml");
-            var configurationPath = "D:\\web\\TesteP3Image\\TesteP3Image\\TesteP3Image\\Models\\NHibernate\\Configuration\\hibernate.cfg.xml";
-            //configuration.Configure(configurationPath.ToString().Replace("Admin", "TesteP3Image"));
+            var configurationPath = HttpContext.Current.Server.MapPath(@"~\Models\NHibernate\Configuration\hibernate.cfg.xml");
+            //var configurationPath = "D:\\web\\TesteP3Image\\TesteP3Image\\TesteP3Image\\Models\\NHibernate\\Configuration\\hibernate.cfg.xml";
             configuration.Configure(configurationPath.ToString().Replace("Publico", "TesteP3Image"));
-            //configuration.Configure(configurationPath.ToString().Replace("Admin.Tests", "TesteP3Image"));
-            var categoriasConfigurationFile = "D:\\web\\TesteP3Image\\TesteP3Image\\TesteP3Image\\Models\\NHibernate\\Mappings\\Categorias.hbm.xml";
-            //var categoriasConfigurationFile = HttpContext.Current.Server.MapPath(@"~\Models\NHibernate\Mappings\Categorias.hbm.xml");
+            //var categoriasConfigurationFile = "D:\\web\\TesteP3Image\\TesteP3Image\\TesteP3Image\\Models\\NHibernate\\Mappings\\Categorias.hbm.xml";
+            var categoriasConfigurationFile = HttpContext.Current.Server.MapPath(@"~\Models\NHibernate\Mappings\Categorias.hbm.xml");
             configuration.AddFile(categoriasConfigurationFile.ToString().Replace("Publico","TesteP3Image"));
-            var subCategoriasConfigurationFile = "D:\\web\\TesteP3Image\\TesteP3Image\\TesteP3Image\\Models\\NHibernate\\Mappings\\SubCategorias.hbm.xml";
-            //var subCategoriasConfigurationFile = HttpContext.Current.Server.MapPath(@"~\Models\NHibernate\Mappings\SubCategorias.hbm.xml");
+            //var subCategoriasConfigurationFile = "D:\\web\\TesteP3Image\\TesteP3Image\\TesteP3Image\\Models\\NHibernate\\Mappings\\SubCategorias.hbm.xml";
+            var subCategoriasConfigurationFile = HttpContext.Current.Server.MapPath(@"~\Models\NHibernate\Mappings\SubCategorias.hbm.xml");
             configuration.AddFile(subCategoriasConfigurationFile.ToString().Replace("Publico", "TesteP3Image"));
-            var camposConfigurationFile = "D:\\web\\TesteP3Image\\TesteP3Image\\TesteP3Image\\Models\\NHibernate\\Mappings\\Campos.hbm.xml";
-            //var camposConfigurationFile = HttpContext.Current.Server.MapPath(@"~\Models\NHibernate\Mappings\Campos.hbm.xml");
+            //var camposConfigurationFile = "D:\\web\\TesteP3Image\\TesteP3Image\\TesteP3Image\\Models\\NHibernate\\Mappings\\Campos.hbm.xml";
+            var camposConfigurationFile = HttpContext.Current.Server.MapPath(@"~\Models\NHibernate\Mappings\Campos.hbm.xml");
             configuration.AddFile(camposConfigurationFile.ToString().Replace("Publico", "TesteP3Image"));
             ISessionFactory sessionFactory = configuration.BuildSessionFactory();
             return sessionFactory.OpenSession();
